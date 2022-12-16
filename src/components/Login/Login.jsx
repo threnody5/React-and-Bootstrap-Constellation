eb0189a350f07193c61ead79a29ac9c1d56aaffb
William <williamw15@protonmail.com>
2022-12-16 12:43:37 -0500

    add login functionality to login screen

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,9 +2,32 @@
 
 import 'bootstrap/dist/css/bootstrap.css';
 import React, { useState } from 'react';
+import { authorization } from '../../FirebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { background } from '../../Background/Background';
 
 export default function Login() {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  const LogInToAccount = () => {
+    signInWithEmailAndPassword(authorization, emailAddress, password)
+      .then((userCredentials) => {
+        //* Signed in
+        const user = userCredentials.user;
+        console.log(user);
+        window.alert('Logged in successfully');
+        setEmailAddress('');
+        setPassword('');
+      })
+      .catch((e) => {
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        window.alert('Wrong email address or password');
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div
       style={{
@@ -21,6 +44,8 @@ export default function Login() {
               type='text'
               className='form-control mt-1'
               placeholder='Email Address'
+              value={emailAddress}
+              onChange={(e) => setEmailAddress(e.target.value)}
             />
           </div>
           <div className='form-group mt-3'>
@@ -29,9 +54,16 @@ export default function Login() {
               type='password'
               className='form-control mt-1'
               placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button className='btn btn-info text-dark mt-4'>Log In</button>
+          <button
+            className='btn btn-info text-dark mt-4'
+            onClick={LogInToAccount}
+          >
+            Log In
+          </button>
           <br />
           <div className='text-white text-center mt-4'>
             Go back to{' '}
