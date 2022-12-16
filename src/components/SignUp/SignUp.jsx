0c305645f3ef785e711e5e55e0025f1c1ffb7a95
William <williamw15@protonmail.com>
2022-12-16 00:31:04 -0500

    accound creation functions with checks in place for email, password length and match

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,8 +1,44 @@
 /** @format */
 
 import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import { authorization } from '../../FirebaseConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 export default function SignUp() {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [reEnterPassword, setReEnterPassword] = useState('');
+
+  const createAccount = () => {
+    if (emailAddress.length < 9) {
+      window.alert('Please enter a valid email address');
+      return;
+    }
+    if (password !== reEnterPassword) {
+      window.alert('Passwords do not match');
+      return;
+    }
+    if (password.length < 8) {
+      window.alert('Please enter a password with at least 8 characters');
+      return;
+    }
+
+    createUserWithEmailAndPassword(authorization, emailAddress, password)
+      .then(() => {
+        //* account is created
+        window.alert('Account has been created');
+        setEmailAddress('');
+        setPassword('');
+        setReEnterPassword('');
+      })
+      .catch((e) => {
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
     <div
       style={{
@@ -27,6 +63,8 @@ export default function SignUp() {
               type='text'
               className='form-control mt-1'
               placeholder='Enter Email'
+              value={emailAddress}
+              onChange={(e) => setEmailAddress(e.target.value)}
             />
           </div>
           <div className='form-group mt-3'>
@@ -35,6 +73,8 @@ export default function SignUp() {
               type='password'
               className='form-control mt-1'
               placeholder='Enter Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className='form-group mt-3'>
@@ -43,12 +83,29 @@ export default function SignUp() {
               type='password'
               className='form-control mt-1'
               placeholder='Re-enter Password'
+              value={reEnterPassword}
+              onChange={(e) => setReEnterPassword(e.target.value)}
             />
+            <div>
+              <button
+                className='btn btn-info text-dark mt-4'
+                onClick={createAccount}
+              >
+                Create Account
+              </button>
+            </div>
           </div>
           <br />
           <div className='text-white text-center'>
-        Go back to {' '}<a className='text-info' href="/">Home</a>{' '} page
-      </div>
+            Go back to{' '}
+            <a
+              className='text-info'
+              href='/'
+            >
+              Home
+            </a>{' '}
+            page
+          </div>
         </div>
       </div>
     </div>
