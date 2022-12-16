a9aade439850dfb8fd2528a09a266a55db59ee63
William <williamw15@protonmail.com>
2022-12-16 10:17:37 -0500

    create sign in form for user

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,39 @@
+/** @format */
 
+import 'bootstrap/dist/css/bootstrap.css';
+import React, { useState } from 'react';
+import { background } from '../../Background/Background';
 
 export default function Login() {
   return (
-    <div>
-      <h1>Login works!</h1>
+    <div
+      style={{
+        backgroundImage: 'url(' + background + ')',
+        height: '100vh',
+      }}
+    >
+      <div className='Auth-container justify-content-center text-center'>
+        <div>
+          <h3 className='text-white text-center'>Log into your Account</h3>
+          <div className='form-group mt-3'>
+            <label className='text-white'>Email Address</label>
+            <input
+              type='text'
+              className='form-control mt-1'
+              placeholder='Email Address'
+            />
+          </div>
+          <div className='form-group mt-3'>
+            <label className='text-white'>Password</label>
+            <input
+              type='password'
+              className='form-control mt-1'
+              placeholder='Password'
+            />
+          </div>
+          <button className='btn btn-info text-dark mt-4'>Log In</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
