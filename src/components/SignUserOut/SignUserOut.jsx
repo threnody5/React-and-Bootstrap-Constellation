3a6d57a823bbbedd9271d1da71879611c458197c
William <williamw15@protonmail.com>
2022-12-18 21:47:33 -0500

    update sign out method to request confirmation from user

--- src/components/SignUserOut/SignUserOut.jsx
+++ src/components/SignUserOut/SignUserOut.jsx
@@ -1,14 +1,24 @@
+/** @format */
+
 import { authorization } from '../../FirebaseConfig';
 import { signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 export const SignUserOut = () => {
-  signOut(authorization)
-    .then(() => {
-      //* Sign out successful
-      window.alert('Successfully signed out');
-    })
-    .catch((e) => {
-      const error = e;
-      window.alert('Error has occurred');
-    })
-}
+  const navigate = useNavigate();
+
+  let userAnswer = window.confirm('Sign out?');
+
+  if (userAnswer === true) {
+    signOut(authorization)
+      .then(() => {
+        //* Sign out successful
+        window.alert('Successfully signed out');
+      })
+      .catch((e) => {
+        const error = e;
+        window.alert('Error has occurred');
+      });
+      navigate('/');
+  }
+};
