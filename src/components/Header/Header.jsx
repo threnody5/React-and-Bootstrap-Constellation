28210e520c11bbde0bc36079ef9a8e932943a03b
William <williamw15@protonmail.com>
2022-12-18 21:46:45 -0500

    update Header to include log out functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,44 +4,53 @@
 // <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
 import 'bootstrap/dist/css/bootstrap.css';
+import { SignUserOut } from '../SignUserOut/SignUserOut';
 
 export default function Header() {
   return (
-      <nav className='navbar navbar-expand-sm fixed-top navbar-dark bg-black'>
-        <div className='container'>
-          <a
-            href='#'
-            className='navbar-brand'
-          >
-            Constellations.Map()
-          </a>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <a
-                href='#'
-                className='nav-link'
-              >Products</a>
-            </li>
-            <li className='nav-item'>
-              <a
-                href='#'
-                className='nav-link'
-              >Shop</a>
-            </li>
-            <li className='nav-item'>
-              <a
-                href='#'
-                className='nav-link'
-              >Contact Us</a>
-            </li>
-            <li className='nav-item'>
-              <a
-                href='#'
-                className='nav-link'
-              >Login</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <nav className='navbar navbar-expand-sm fixed-top navbar-dark bg-black'>
+      <div className='container'>
+        <a
+          href='#'
+          className='navbar-brand'
+        >
+          Constellations.Map()
+        </a>
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <a
+              href='#'
+              className='nav-link'
+            >
+              Constellations
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              href='#'
+              className='nav-link'
+            >
+              Planets
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              href='#'
+              className='nav-link'
+            >
+              Contact Us
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a
+              onClick={SignUserOut}
+              className='nav-link'
+            >
+              Log Out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
