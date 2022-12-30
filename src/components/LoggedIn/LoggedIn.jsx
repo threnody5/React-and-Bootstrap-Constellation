9a2c2f5835120cba237cac777d857b2ec07ee623
William <williamw15@protonmail.com>
2022-12-29 22:29:22 -0500

    add imports, retrieve state containing uid from Login component, retieve user data from database on load, and add conditional rendering if database data is loaded or not

--- src/components/LoggedIn/LoggedIn.jsx
+++ src/components/LoggedIn/LoggedIn.jsx
@@ -1,11 +1,31 @@
 /** @format */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import { background } from '../../Background/Background';
+import { useLocation } from 'react-router-dom';
+import { ref, onValue } from 'firebase/database';
+import { database } from '../../FirebaseConfig';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../../App.css';
 
 export default function LoggedIn() {
-  // const { token } = props;
+  const { state } = useLocation();
+  const [serverData, setServerData] = useState();
+  const [populatedServerData, setPopulatedServerData] = useState(false);
+
+  useEffect(() => {
+    const retrieveData = ref(database, 'users/' + state + '/constellationData');
+    onValue(retrieveData, (snapshot) => {
+      const data = snapshot.val();
+      setServerData(data);
+      setPopulatedServerData(true);
+    });
+  }, []);
+
+  console.log(serverData);
 
   return (
     <div
@@ -16,6 +36,31 @@ export default function LoggedIn() {
     >
       <div>
         <Header />
+      </div>
+      <div>
+        {!populatedServerData && (
+          <div>
+            <h1 className='loading-text'>Loading constellations from Database...</h1>
+          </div>
+        )}
+        {populatedServerData &&
+          serverData.map((item, key) => (
+            <Card
+            border='dark'
+              className='card-container'
+              style={{ width: '18rem' }}
+            >
+              <Card.Img
+                variant='top'
+                src={{ uri: item.url }}
+              />
+              <Card.Body>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.Text>{item.information}</Card.Text>
+                <Button variant='primary'>Viewed</Button>
+              </Card.Body>
+            </Card>
+          ))}
       </div>
     </div>
   );
