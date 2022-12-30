0b021199e7e3ec14c16201e3e5c2c1a21c142523
William <williamw15@protonmail.com>
2022-12-29 22:27:20 -0500

    import css stylesheet and add navbar-header class to outside div

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,12 @@
 // <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
 
 import 'bootstrap/dist/css/bootstrap.css';
+import '../../App.css';
 import { SignUserOut } from '../SignUserOut/SignUserOut';
 
 export default function Header() {
   return (
-    <nav className='navbar navbar-expand-sm fixed-top navbar-dark bg-black'>
+    <nav className='navbar-header navbar navbar-expand-sm fixed-top navbar-dark bg-black'>
       <div className='container'>
         <a
           href='#'
