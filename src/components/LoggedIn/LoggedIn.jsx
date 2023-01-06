04a1a1eb6a080c98cfb1ae8658d1e3b85f420c51
William <williamw15@protonmail.com>
2023-01-06 15:26:28 -0500

    attempt to add checks to ensure the user is logged in to view the loggedin page, if not than redirect to login screen

--- src/components/LoggedIn/LoggedIn.jsx
+++ src/components/LoggedIn/LoggedIn.jsx
@@ -3,29 +3,45 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import { background } from '../../Background/Background';
-import { useLocation } from 'react-router-dom';
+import { useLocation, redirect, useNavigate } from 'react-router-dom';
 import { ref, onValue } from 'firebase/database';
 import { database } from '../../FirebaseConfig';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import AuthGuard from '../AuthGuard/AuthGuard';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../App.css';
 
 export default function LoggedIn() {
   const { state } = useLocation();
+
+  const navigate = useNavigate();
+
+  // const currentUserStatus = state;
+  // console.log(currentUserStatus);
+  // const authGuard = new AuthGuard;
   const [serverData, setServerData] = useState();
   const [populatedServerData, setPopulatedServerData] = useState(false);
+  let [userStatus, setUserStatus] = useState(0);
 
   useEffect(() => {
+    setUserStatus(state);
     const retrieveData = ref(database, 'users/' + state + '/constellationData');
     onValue(retrieveData, (snapshot) => {
       const data = snapshot.val();
       setServerData(data);
       setPopulatedServerData(true);
+      // console.log(`User ${userStatus}`);
     });
   }, []);
 
-  console.log(serverData);
+  if (parseInt(state.length === 0)) {
+    // console.log(state);
+    return navigate('/login');
+    // console.log(`User ${userStatus}`);
+  }
+
+  // console.log(userStatus);
 
   return (
     <div
@@ -37,16 +53,23 @@ export default function LoggedIn() {
       <div>
         <Header />
       </div>
+      {/* <div>
+      <AuthGuard userStatus={userStatus} />
+      </div> */}
       <div>
         {!populatedServerData && (
           <div>
-            <h1 className='loading-text'>Loading constellations from Database...</h1>
+            <h1 className='loading-text'>
+              Loading constellations from Database...
+            </h1>
           </div>
         )}
         {populatedServerData &&
+          userStatus !== 0 &&
           serverData.map((item, key) => (
             <Card
-            border='dark'
+              key={key}
+              border='dark'
               className='card-container'
               style={{ width: '18rem' }}
             >
